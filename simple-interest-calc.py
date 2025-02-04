66ca21e9cd65dd657735a04312a576d751d2afed
hdhillon23 <harvird106@deltalearns.ca>
2025-02-04 11:33:04 -0800

    Task 2
    Almost completed

---
+++ simple-interest-calc.py
@@ -0,0 +1,16 @@
+# TASK: Create a program that calculates simple interest.
+
+# Ask the user for their name and greet them
+name = input("What is your name? ").title().strip()
+print(f"Hi {name}! Welcome to the simple interest calculator program!")
+
+while True:
+# Prompt the user to enter the principal amount
+
+# Prompt the user to enter the annual interest rate (in percentage)
+
+# Prompt the user to enter the time period in years
+
+# Calculate the simple interest using the formula
+
+# Display the calculated simple interest
