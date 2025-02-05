a1e8f68b902e539b42cba503f1e3deb4901fa280
hdhillon23 <harvird106@deltalearns.ca>
2025-02-05 10:20:37 -0800

    Task 3
    Completed task 3

--- area-of-a-rectangle.py
+++ area-of-a-rectangle.py
@@ -4,16 +4,25 @@
 name = input("What is your name? ").title().strip()
 print(f"Hi {name}! Welcome to the area of a rectangle calculator program!")
 
-# Prompt the user to enter the length of the rectangle
-length = float(input(f"{name} please enter the length of your rectangle "))
+while True:
+    # Prompt the user to enter the length of the rectangle
+    length = float(input(f"{name} please enter the length of your rectangle "))
 
-# Prompt the user to enter the width of the rectangle
+    # Prompt the user to enter the width of the rectangle
+    width = float(input(f"{name} please enter the width of your rectangle "))
 
+    # Calculate the area using the formula: length * width
+    area = (length * width)
 
-# Calculate the area using the formula: length * width
+    # Display the calculated area
+    print(f"{name} the area of your rectangle is {area}" )
 
+     # Ask the user if they want to do it again
+    choice = input(f" {name} would you like to do another area of a rectangle calculation [y/n]? ").strip().lower()
+    if choice == "y":
+        pass
+    else:
+        break
 
-
-# Display the calculated area
-
-
+ # Say goodbye
+print(f"Thanks for using my program {name}!")
