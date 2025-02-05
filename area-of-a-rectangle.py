a30ba9b1d8d69e57a0ea438b39e124298361aaab
hdhillon23 <harvird106@deltalearns.ca>
2025-02-05 10:12:00 -0800

    Task 2 and 3
    Task 2 is completed almost done task 3

---
+++ area-of-a-rectangle.py
@@ -0,0 +1,19 @@
+# TASK: Create a program that calculates the area of a rectangle.
+
+# Ask the user for their name and greet them
+name = input("What is your name? ").title().strip()
+print(f"Hi {name}! Welcome to the area of a rectangle calculator program!")
+
+# Prompt the user to enter the length of the rectangle
+length = float(input(f"{name} please enter the length of your rectangle "))
+
+# Prompt the user to enter the width of the rectangle
+
+
+# Calculate the area using the formula: length * width
+
+
+
+# Display the calculated area
+
+
--- simple-interest-calc.py
+++ simple-interest-calc.py
@@ -5,12 +5,28 @@ name = input("What is your name? ").title().strip()
 print(f"Hi {name}! Welcome to the simple interest calculator program!")
 
 while True:
-# Prompt the user to enter the principal amount
+    # Prompt the user to enter the principal amount
+    print(f"{name} this is a simple interest calculator")
+    principle_amount = float(input(f"{name} please enter the principle amount "))
 
-# Prompt the user to enter the annual interest rate (in percentage)
+    # Prompt the user to enter the annual interest rate (in percentage)
+    anual_interest_rate = float(input(f"{name }please enter the annual interest rate (In percentage) "))
+   
+    # Prompt the user to enter the time period in years
+    num_years = float(input(f"{name} please enter the time period in years"))
 
-# Prompt the user to enter the time period in years
+    # Calculate the simple interest using the formula
+    intrest = (principle_amount * anual_interest_rate * num_years) / 10
 
-# Calculate the simple interest using the formula
+    # Display the calculated simple interest
+    print(f"{name} your interest will be {intrest} ")
 
-# Display the calculated simple interest
+    # Ask the user if they want to do it again
+    choice = input(f" {name} would you like to do another simple interest calculation [y/n]? ").strip().lower()
+    if choice == "y":
+        pass
+    else:
+        break
+
+ # Say goodbye
+print(f"Thanks for using my program {name}!")
